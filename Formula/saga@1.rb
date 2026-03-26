360dd877e060aa7a1e0f410e3f7f020de6c54a16
Kevin Renskers <kevin@loopwerk.io>
2026-03-26 19:46:33 +0100

    Update saga@1 to 1.4.0

--- Formula/saga@1.rb
+++ Formula/saga@1.rb
@@ -1,7 +1,7 @@
 class SagaAT1 < Formula
   desc "A code-first static site generator written in Swift"
   homepage "https://github.com/loopwerk/saga-cli"
-  url "https://github.com/loopwerk/saga-cli.git", tag: "1.1.0"
+  url "https://github.com/loopwerk/saga-cli.git", tag: "1.4.0"
   license "MIT"
 
   depends_on xcode: ["13.0", :build]
