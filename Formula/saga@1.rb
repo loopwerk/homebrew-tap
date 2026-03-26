5ac909e08a3d298595c520bdb6f43189a2d661a7
Kevin Renskers <kevin@loopwerk.io>
2026-03-26 17:47:45 +0100

    Add a formula for saga-cli 1.1.0

---
+++ Formula/saga@1.rb
@@ -0,0 +1,18 @@
+class SagaAT1 < Formula
+  desc "A code-first static site generator written in Swift"
+  homepage "https://github.com/loopwerk/saga-cli"
+  url "https://github.com/loopwerk/saga-cli.git", tag: "1.1.0"
+  license "MIT"
+
+  depends_on xcode: ["13.0", :build]
+  depends_on macos: :monterey
+
+  def install
+    system "swift", "build", "-c", "release", "--product", "saga", "--disable-sandbox"
+    bin.install ".build/release/saga"
+  end
+
+  test do
+    system bin/"saga", "--help"
+  end
+end
